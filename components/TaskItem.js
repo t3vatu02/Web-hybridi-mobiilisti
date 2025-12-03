bf3d4900b60b5df8bf42a9090e9773cd38852c95
t3vatu02 <tuureracing@gmail.com>
2025-12-03 21:14:49 +0200

    Add files via upload

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function TaskItem({ task, toggleTask }) {
+export default function TaskItem({ task, toggleTask, removeTask }) {
   return (
-    <TouchableOpacity onPress={() => toggleTask(task.id)}>
+    <TouchableOpacity
+      onPress={() => toggleTask(task.id)}
+      onLongPress={() => removeTask(task.id)}
+    >
       <Text style={[styles.text, task.done && styles.done]}>
         {task.text}
       </Text>
